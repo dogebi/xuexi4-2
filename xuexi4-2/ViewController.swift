2c107d8b5a0cb5f8822bc2a567a02b9962853ab7
long <long@long-ui-MacBook-Pro.local>
2017-04-01 16:12:07 +0900

    Initial Commit

---
+++ xuexi4-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xuexi4-2
+//
+//  Created by long on 2017. 4. 1..
+//  Copyright © 2017년 heoyonghwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ xuexi4-2Tests/xuexi4_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  xuexi4_2Tests.swift
+//  xuexi4-2Tests
+//
+//  Created by long on 2017. 4. 1..
+//  Copyright © 2017년 heoyonghwan. All rights reserved.
+//
+
+import XCTest
+@testable import xuexi4_2
+
+class xuexi4_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
